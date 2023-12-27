3bf710fb730db62c2956210ed1a3ba3dfa86d4e8
maxsf1 <max.schwarzbach@stud.hn.de>
2023-12-27 11:26:35 +0100

    Testbeispiel gefixt, Restcontroller fehlte
    Ohne Restcontroller keine Rest Anbindung möglich (Fehler auf der Quick-Guide Seite von Spring-Boot
    
    Co-Authored-By: Lütz, Joachim <147558875+Blumenkohl4152@users.noreply.github.com>

--- KanbanmanagementService/KanbanmanagementService/src/main/java/com/KanbanManagement/KanbanmanagementService/KanbanmanagementServiceApplication.java
+++ KanbanmanagementService/KanbanmanagementService/src/main/java/com/KanbanManagement/KanbanmanagementService/KanbanmanagementServiceApplication.java
@@ -2,10 +2,7 @@ package com.KanbanManagement.KanbanmanagementService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @SpringBootApplication
 public class KanbanmanagementServiceApplication {
@@ -13,9 +10,4 @@ public class KanbanmanagementServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KanbanmanagementServiceApplication.class, args);
 	}
-	
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-    }
 }
