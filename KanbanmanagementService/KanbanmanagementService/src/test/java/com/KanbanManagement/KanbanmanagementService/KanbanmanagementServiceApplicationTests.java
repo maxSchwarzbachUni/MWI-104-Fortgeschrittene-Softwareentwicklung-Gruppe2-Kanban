42364a93f5bdcae31e79bdc8fd3491d69aecc886
maxsf1 <max.schwarzbach@stud.hn.de>
2023-12-27 11:02:58 +0100

    Initiales Projekt angelegt mittels Spring Initilzr
    Co-Authored-By: Lütz, Joachim <147558875+Blumenkohl4152@users.noreply.github.com>

---
+++ KanbanmanagementService/KanbanmanagementService/src/test/java/com/KanbanManagement/KanbanmanagementService/KanbanmanagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KanbanManagement.KanbanmanagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanmanagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
